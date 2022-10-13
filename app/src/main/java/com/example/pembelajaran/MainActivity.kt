267fafa234ca4e1740832a428847d9217d80878d
zainulfakhri16 <zainul.fakhri2002@gmail.com>
2022-10-14 06:25:19 +0800

    navigator menu

--- app/src/main/java/com/example/pembelajaran/MainActivity.kt
+++ app/src/main/java/com/example/pembelajaran/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.pembelajaran
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.nav_header, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.i_upload-> Toast.makeText(applicationContext, "ini adalah upload",Toast.LENGTH_SHORT).show()
+            R.id.i_like -> Toast.makeText(applicationContext, "ini adalah like",Toast.LENGTH_SHORT).show()
+            R.id.i_dm -> Toast.makeText(applicationContext, "ini adalah dm",Toast.LENGTH_SHORT).show()
+            else -> return super.onOptionsItemSelected(item)
+        }
+      return true
     }
 }
